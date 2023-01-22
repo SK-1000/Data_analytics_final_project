83692d1b807a9dcbb418fb4eb9b4662a105c7560
sk1001 <sheilafleming81@gmail.com>
2023-01-22 18:52:44 +0000

    changed to multipage and adding plotly express

--- streamlit project/main.py
+++ streamlit project/main.py
@@ -1,25 +1,47 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly_express as px
 
-st.title("Event Data Analysis Tool")
-st.write("use current trends to visual further opportunities")
-st.text("just input your data and this app will do the rest")
-st.markdown('This is a **bold**')
-st.markdown('## This is **level two header with bold**')
 
-uploaded_file = st.file_uploader('Upload your file here')
 
-if uploaded_file:
+def stats(dataframe):
     st.header('Data Statistics')
-    df = pd.read_csv(uploaded_file)
-    st.write(df.describe())
-    st.header ('Data Header')
-    st.write(df.head())
+    st.write(dataframe.describe())
 
+def data_header(dataframe):
+    st.header ('Data Header')
+    st.write(dataframe.head())
+
+def plot(dataframe):
     fig, ax = plt.subplots(1,1)
     ax.scatter(x=df['Order Timestamp'], y=df['Total Price Paid Per Ticket'])
     ax.set_xlabel('Order Timestamp')
     ax.set_ylabel('Total Price Paid Per Ticket')
 
     st.pyplot(fig)
+
+
+st.title("Event Data Analysis Tool")
+st.write("use current trends to visual further opportunities")
+st.text("just input your data and this app will do the rest")
+# st.markdown('This is a **bold**')
+# st.markdown('## This is **level two header with bold**')
+
+st.sidebar.title('Navigation')
+
+uploaded_file = st.sidebar.file_uploader('Upload your file here')
+
+options = st.sidebar.radio('Pages', options=['Home', 'Data Statistics', 'Data Header', 'Plot'])
+
+if uploaded_file:
+ 
+    df = pd.read_csv(uploaded_file)
+    
+   
+if options == 'Data Statistics':
+    stats(df)
+elif options == 'Data Header':
+    data_header(df)
+elif options == 'Plot':
+    plot(df)
