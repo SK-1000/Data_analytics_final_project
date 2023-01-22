4645caaab6463e89f06a4381df79692efb45a728
sk1001 <sheilafleming81@gmail.com>
2023-01-22 19:46:25 +0000

    Added interative plot with plotly express

--- streamlit project/main.py
+++ streamlit project/main.py
@@ -22,6 +22,18 @@ def plot(dataframe):
     st.pyplot(fig)
 
 
+def interactive_plot(dataframe):
+    x_axis_val = st.selectbox('Select X-Axis Value', options=df.columns)
+    y_axis_val = st.selectbox('Select Y-Axis Value', options=df.columns)
+    col = st.color_picker('Pick a Plot Colour')
+    
+
+    plot = px.scatter(dataframe, x=x_axis_val, y=y_axis_val)
+    plot.update_traces(marker=dict(color=col))
+    st.plotly_chart(plot)
+
+
+
 st.title("Event Data Analysis Tool")
 st.write("use current trends to visual further opportunities")
 st.text("just input your data and this app will do the rest")
@@ -32,7 +44,7 @@ st.sidebar.title('Navigation')
 
 uploaded_file = st.sidebar.file_uploader('Upload your file here')
 
-options = st.sidebar.radio('Pages', options=['Home', 'Data Statistics', 'Data Header', 'Plot'])
+options = st.sidebar.radio('Pages', options=['Home', 'Data Statistics', 'Data Header', 'Plot', 'Interactive Plot'])
 
 if uploaded_file:
  
@@ -45,3 +57,5 @@ elif options == 'Data Header':
     data_header(df)
 elif options == 'Plot':
     plot(df)
+elif options == 'Interactive Plot':
+    interactive_plot(df)
