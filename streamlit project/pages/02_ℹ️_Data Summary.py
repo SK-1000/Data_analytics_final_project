195d90829faac362e8770e0fc9dbd52bd6274d08
sk1001 <sheilafleming81@gmail.com>
2023-03-02 21:55:59 +0000

    merged from branch to master

--- streamlit project/pages/02_ℹ️_Data Summary.py
+++ streamlit project/pages/02_ℹ️_Data Summary.py
@@ -3,6 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
+import plotly.express as px
+import datetime as dt
+import plotly.io as pio
+
+
 
 #removes the default burger menu
 hide_default_format = """
@@ -39,15 +44,44 @@ if uploaded_file is not None:
     df.loc[df.index == inputDate ] 
     # I would like to add validation so that for both search boxes if not thing is required leave a message
     
-  
+    df['Booking Date'] = pd.to_datetime(df['Booking Date']).dt.strftime("%m/%d/%Y") 
+    # df['Booking Date'] = pd.to_datetime(df['Booking Date']).dt.strftime("%d/%m/%Y")  #changing the order Booking Date to be specified format
+    county_choice = df['County'].unique().tolist()
+
+    st.write(df)
+
+    
+    date_choice = df['Booking Date'].unique().tolist()
+    date = st.selectbox('Which date would you like to see', date_choice, 100)
+    county = st.multiselect('Which county would you like to see?', county_choice, ['Dublin'])
+
+    df = df[df['County'].isin(county)]
+    # df = df[df['Booking Date']==date] removing the filtering because I'm adding animation
+
+    sumCount = df['Participant Count'].sum()
+    st.write(sumCount)
+
+
+    fig1 = px.histogram(df, x='County', y='Participant Count', color='County',
+        range_y=[0,100], animation_frame='Booking Date', animation_group='County') #needed to use a histogram here rather than a bar chart because I needed to aggregate the data.
+
+
+    fig1.update_layout(width=800)
+    st.write(fig1)
 
 
 
-    st.header ('Plotting orders per date')
-    fig1, ax = plt.subplots(figsize=(12,6), layout='constrained')
-    dates = df.index
-    # data = df['barcode'].value_counts
-    data = np.cumsum(np.random.randn(len(dates)))
+
+
+
+
+
+
+    # st.header ('Plotting orders per date')
+    # fig1, ax = plt.subplots(figsize=(12,6), layout='constrained')
+    # dates = df.index
+    # # data = df['barcode'].value_counts
+    # data = np.cumsum(np.random.randn(len(dates)))
 
   
 
