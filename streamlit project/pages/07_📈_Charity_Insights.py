4a77e5a6701f125ba1e3652b295098be0158a638
sk1001 <sheilafleming81@gmail.com>
2023-03-22 20:08:22 +0000

    update columns on charity page

--- streamlit project/pages/07_📈_Charity_Insights.py
+++ streamlit project/pages/07_📈_Charity_Insights.py
@@ -23,55 +23,51 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
     
 # is file uploaded
 if df is not None:
-
-    
-    totalRaised = (df['Raised'].sum())
-    formatTotalRaised = "€{:,.2f}".format(totalRaised)
-    st.write("The total raised for Charity based on this file data is:")
-    st.write(formatTotalRaised)
-
-    st.write("The Maximum amount raised by one person is:")
-    maxAmountRaised = df['Raised'].max()
-    formatmaxAmountRaised = "€{:,.2f}".format(maxAmountRaised)
-    st.write(formatmaxAmountRaised)
-
-    st.write("This Amount was raised by:")
-    
-    maxAmountRaised1 = df.loc[df['Raised'] == df['Raised'].max(), 'First Name'].unique()
-    maxAmountRaised2 = df.loc[df['Raised'] == df['Raised'].max(), 'Last Name'].unique()
-
-    st.write("First Name:")
-    st.dataframe(maxAmountRaised1)
-    st.write("Last Name:")
-    st.dataframe(maxAmountRaised2)
-  
-    year_choice = df['Booking Year'].unique().tolist()
-    # year = st.selectbox('Select a Year', year_choice, 0) #default is zero
-    # df = df[df['Booking Year']==year] # filter the data based on the year
-
     st.header('Charity amounts raised displayed by participant age per year')
     fig = px.scatter(df, x="Raised", y="Age", size="Profit Per Ticket", color="Event Name", hover_name="Event Name", log_x=True, size_max=55, range_x=[5,10000],
     range_y=[14,110], animation_frame="Booking Year", animation_group="County")
     fig.update_layout(width=800)
     st.write(fig)
+    
+    left, right = st.columns(2)   
+    with left:     
+        totalRaised = (df['Raised'].sum())
+        formatTotalRaised = "€{:,.2f}".format(totalRaised)
+        st.metric("The total raised for Charity based on this file data is:", formatTotalRaised)
 
-
-    st.header('Amount Raised per Charity')
-    raisedPerCharityTable = df.pivot_table(index='Charity', values=['Raised'], aggfunc='sum')
-    st.dataframe(raisedPerCharityTable)
-
-    st.header('Amount Raised per Charity per Event')
-    raisedPerCharityTable = df.pivot_table(index=('Event Name','Charity'), values=['Raised'], aggfunc='sum')
-    st.dataframe(raisedPerCharityTable)
-
-    st.header('Plot of Amounts Raised per Participant')
-    #The majority of participants are between the ages of approx 35 and 65 with a few outliers
-    fig, ax = plt.subplots()
-    df['Raised'].plot(kind='box', vert=False, figsize=(14,6))
-    st.pyplot(fig)
+        st.header('Amount Raised per Charity')
+        raisedPerCharityTable = df.pivot_table(index='Charity', values=['Raised'], aggfunc='sum')
+        st.dataframe(raisedPerCharityTable)
 
 
 
+        st.header('Amount Raised per Charity per Event')
+        raisedPerCharityTable = df.pivot_table(index=('Event Name','Charity'), values=['Raised'], aggfunc='sum')
+        st.dataframe(raisedPerCharityTable)
+
+
+    with right:  
+
+        st.write("The Maximum amount raised by one person is:")
+        maxAmountRaised = df['Raised'].max()
+        formatmaxAmountRaised = "€{:,.2f}".format(maxAmountRaised)
+        st.write(formatmaxAmountRaised)
+
+        st.write("This outstanding amount was raised by:")
+        maxAmountRaised1 = df.loc[df['Raised'] == df['Raised'].max(), 'First Name'].unique()
+        maxAmountRaised2 = df.loc[df['Raised'] == df['Raised'].max(), 'Last Name'].unique()
+
+        st.write("First Name:")
+        st.dataframe(maxAmountRaised1)
+        st.write("Last Name:")
+        st.dataframe(maxAmountRaised2)
+  
+
+        st.header('Plot of Amounts Raised per Participant')
+         #The majority of participants are between the ages of approx 35 and 65 with a few outliers
+        fig, ax = plt.subplots()
+        df['Raised'].plot(kind='box', vert=False, figsize=(14,6))
+        st.pyplot(fig)
 
 else:
     # st.write("REMEMBER TO UPLOAD A FILE IN ORDER TO VIEW DATA")
